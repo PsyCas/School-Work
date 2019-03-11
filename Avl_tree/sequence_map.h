6d0bbf55c7b8a20b23637f6a59bfef2cc94b6eec
Parakram Basnet <parakram.basnet@gmail.com>
2019-03-11 15:14:38 -0400

    More meaningful documentation

--- Avl_tree/sequence_map.h
+++ Avl_tree/sequence_map.h
@@ -35,16 +35,22 @@ class SequenceMap{
         // precondition: recognition_sequence_ ==  other_sequence_.recognition_sequence_
         void merge(const SequenceMap &other_sequence_){
 
+            // flag that checks if there are duplicates or not
             bool canMerge = true;
 
+            //looping through every element in other_sequence
             for(string enzo_acro : other_sequence_.enzyme_acronyms_){
+                
+                // looping through every iteration in enzyme_acronyms_
                 for(string individual_enzyme : enzyme_acronyms_){
                     
+                    // avoiding duplicates by setting flag to false
                     if(individual_enzyme == enzo_acro){
                         canMerge = false;
                     }
                 }
-                
+
+                // only merges if the flag is set to true
                 if(canMerge) enzyme_acronyms_.push_back(enzo_acro);
             }
         }
