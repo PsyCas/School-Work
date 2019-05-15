dbe2a5e2a6192906fcbb5bf3dcc864df2290a4fa
Parakram Basnet <parakram.basnet@gmail.com>
2019-05-15 16:22:05 -0400

    Added the code for the matrix class from the sample code in the textbook

--- Dynamic_matrix_multiplication/matrix.h
+++ Dynamic_matrix_multiplication/matrix.h
@@ -1,10 +1,43 @@
-#include <iostream>
+#ifndef MATRIX_H2
+#define MATRIX_H34
+
+#include <vector>
 
 using namespace std;
 
-template <typename T>
+template <typename Object>
+
 class matrix{
-
-
-
+    public:
+        
+        matrix( int rows, int cols ) : array( rows )
+        {
+            for( auto & thisRow : array )
+                thisRow.resize( cols );
+        }
+        
+        matrix( vector<vector<Object>>v ) : array{ v }
+        {}
+        
+        matrix( vector<vector<Object>> &&v):array{ std::move( v ) }
+        {}
+        
+        const vector<Object> & operator[]( int row ) const
+        { return array[ row ]; }
+        
+        vector<Object> & operator[]( int row )
+        
+        { return array[ row ]; }
+        
+        int numrows( ) const
+        { return array.size( ); }
+        
+        int numcols( ) const
+        { return numrows( ) ? array[ 0 ].size( ) : 0; }
+        
+    private:
+        
+        vector<vector<Object>> array;
 };
+    
+#endif
