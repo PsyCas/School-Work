0691d843f0e0f22e9cec572b40196b36720cd479
PsyCas <parakram.basnet@gmail.com>
2020-03-14 23:20:58 -0400

    related files

---
+++ Computational Vision/Assignment 3- Hough Complex/p1.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "image.h"
+
+using namespace std;
+using namespace ComputerVisionProjects;
+
+int main(int argc, char** argv){
+
+  if(argc != 3){
+    cout << "Usage: %s <grey level image> <output grey-level edge image>\n";
+    return 0;
+  }
+
+
+
+
+  return 0;
+}
