d50ca552d3b15398ec3e266472a8aa4a52594faa
PsyCas <parakram.basnet@gmail.com>
2020-03-17 13:47:29 -0400

    init h3

--- Computational Vision/Assignment 3- Hough Complex/h3.cc
+++ Computational Vision/Assignment 3- Hough Complex/h3.cc
@@ -4,15 +4,22 @@
 using namespace std;
 using namespace ComputerVisionProjects;
 
+
+
+
+
 int main(int argc, char **argv){
 
   if(argc != 4){
-    cout << "Usage: %s <grey level image> <grey level threshold> <output binary image>\n";
+    cout << "Usage: %s <input binary image> <grey level hough image> <output Hough voting array>\n";
     return 0;
   }
 
   const string input_file(argv[1]);
-  const string output_file(argv[3]);
+  const string output_file(argv[2]);
+  const string output_array(argv[3]);
+
+  cout << input_file << " " << output_file << " " << output_array << endl;
 
   return 0;
 }
