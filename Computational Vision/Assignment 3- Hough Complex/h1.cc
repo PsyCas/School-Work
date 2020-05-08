d5dc0f8620678e4525ac93fc6190fcee430b8078
PsyCas <parakram.basnet@gmail.com>
2020-05-08 18:06:21 -0400

    3

--- Computational Vision/Assignment 3- Hough Complex/h1.cc
+++ Computational Vision/Assignment 3- Hough Complex/h1.cc
@@ -6,51 +6,83 @@
 using namespace std;
 using namespace ComputerVisionProjects;
 
-void calculateImage(const Image& input_image, const vector<vector<int>> kernel, Image& return_img){
+double calculateSobel(const int &row, const int &col, const Image& input_image, const vector<vector<int>> kernelX, const vector<vector<int>> kernelY){
 
-  for(int i = 0; i < input_image.num_rows(); ++i){
+  double totalX = 0, totalY = 0;
+
+  // getting the value of pixels from the image
+  int topLeft = (row != 0 && col != 0)? input_image.GetPixel(row-1, col-1): 0;
+  totalX += topLeft * kernelX[0][0];
+  totalY += topLeft * kernelY[0][0];
+  
+  int top = row != 0? input_image.GetPixel(row-1, col): 0;
+  totalX += top * kernelX[0][1];
+  totalY += top * kernelY[0][1];
+  
+
+  int topRight = (row != 0 && col != input_image.num_columns()-1)? input_image.GetPixel(row-1, col+1): 0;
+  totalX += topRight * kernelX[0][2];
+  totalY += topRight * kernelY[0][2];
+  
+  
+  int left = col != 0? input_image.GetPixel(row, col-1): 0;
+  totalX += left * kernelX[1][0];
+  totalY += left * kernelY[1][0];
+  
+
+  int center = input_image.GetPixel(row, col);
+  totalX += center * kernelX[1][1];
+  totalY += center * kernelY[1][1];
+  
+  int right = col != input_image.num_columns()-1? input_image.GetPixel(row, col+1): 0;
+  totalX += right * kernelX[1][2];
+  totalY += right * kernelY[1][2];
+  
+ 
+  int bottomLeft = (row != input_image.num_rows()-1 && col != 0)? input_image.GetPixel(row+1, col-1): 0;
+  totalX += bottomLeft * kernelX[2][0];
+  totalY += bottomLeft * kernelY[2][0];
+  
+
+  int bottom = row != input_image.num_rows()-1? input_image.GetPixel(row+1, col): 0;
+  totalX += bottom * kernelX[2][1];
+  totalY += bottom * kernelY[2][1];
+  
+
+  int bottomRight = (row != input_image.num_rows()-1 && col != input_image.num_columns()-1)? input_image.GetPixel(row+1, col+1): 0;
+  totalX += bottomRight * kernelX[2][2];
+  totalY += bottomRight * kernelY[2][2];
+  
+  return sqrt(pow(totalX,2) + pow(totalY, 2));
+}
+
+void calculateImage(const Image& input_image, vector<vector<double>> &imageVec, double& maxVal){
+
+  // kernel for sobel operator, x and y axis
+  vector<vector<int>> kernelX = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+  vector<vector<int>> kernelY = {{1, 2, 1}, {0, 0, 0}, {-1, -2, -1}};
+ 
+  vector<double> currentValue;
+  currentValue.reserve(input_image.num_columns());
+
+  for(int i = 0; i < input_image.num_rows(); ++i){  
     for(int j = 0; j < input_image.num_columns(); ++j){            
       
-      // getting the value of pixels from the image
-      int topLeft = (i != 0 && j != 0)? input_image.GetPixel(i-1, j-1): 0;
-      int top = i != 0? input_image.GetPixel(i-1, j): 0;
-      int topRight = (i != 0 && j != input_image.num_columns()-1)? input_image.GetPixel(i-1, j+1): 0;
-      
-      int left = j != 0? input_image.GetPixel(i, j-1): 0;
-      int center = input_image.GetPixel(i, j);
-      int right = j != input_image.num_columns()-1? input_image.GetPixel(i, j+1): 0;
-      
-      int bottomLeft = (i != input_image.num_rows()-1 && j != 0)? input_image.GetPixel(i+1, j-1): 0;
-      int bottom = i != input_image.num_rows()-1? input_image.GetPixel(i+1, j): 0;
-      int bottomRight = (i != input_image.num_rows()-1 && j != input_image.num_columns()-1)? input_image.GetPixel(i+1, j+1): 0;
-
-      // multiplying the value of pixels to the kernel
-      topLeft = topLeft * kernel[0][0];
-      top = top * kernel[0][1]; 
-      topRight = topRight * kernel[0][2];
-
-      left = left * kernel[1][0];
-      center = center * kernel[1][1];
-      right = right * kernel[1][2];
-
-      bottomLeft = bottomLeft * kernel[2][0];
-      bottom = bottom * kernel[2][1];
-      bottomRight = bottomRight * kernel[2][2];
-
-      int sum = topLeft + top + topRight + left + center + right + bottomLeft + bottom + bottomRight;
-      return_img.SetPixel(i, j, sum);
+      double labelVal = calculateSobel(i, j, input_image, kernelX, kernelY);
+      maxVal = labelVal > maxVal? labelVal: maxVal;
+      currentValue.emplace_back(labelVal);
     }
+    imageVec.emplace_back(std::move(currentValue));
   }
 }
 
-void getFinalImage(Image& input_image, const Image& xImg, const Image& yImg){
+void getFinalImage(Image& input_image, vector<vector<double>> imageVec, const double& maxVal){
 
-    for(int i = 0; i < input_image.num_rows(); ++i){
-      for(int j = 0; j < input_image.num_columns(); ++j){            
-        int value = round(sqrt(pow(xImg.GetPixel(i,j),2) + pow(yImg.GetPixel(i,j),2))); // newPixel = sqrt(xPxl^2 + ypxl^2)
-        input_image.SetPixel(i, j, value);
-      }
+  for(int i = 0; i < input_image.num_rows(); ++i){
+    for(int j = 0; j < input_image.num_columns(); ++j){            
+      input_image.SetPixel(i, j, (imageVec[i][j]/maxVal) * 255);
     }
+  }
 }
 
 
@@ -72,25 +104,12 @@ int main(int argc, char** argv){
       return 0;
   }
   
-  
-  // kernel for sobel operator, x and y axis
-  vector<vector<int>> kernelX = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
-  vector<vector<int>> kernelY = {{1, 2, 1}, {0, 0, 0}, {-1, -2, -1}};
+  double maxVal = 0;
+  vector<vector<double>> imageVec;
+  imageVec.reserve(an_image.num_rows() * an_image.num_columns());
 
-  Image xImg, yImg;
-  if (!ReadImage(input_file, &xImg)) {
-      cout <<"Can't open file " << input_file << endl;
-      return 0;
-  }
-  if (!ReadImage(input_file, &yImg)) {
-      cout <<"Can't open file " << input_file << endl;
-      return 0;
-  }
-
-  calculateImage(an_image, kernelX, xImg); // x kernel 
-  calculateImage(an_image, kernelY, yImg); // y kernel
-  
-  getFinalImage(an_image, xImg, yImg);  //final Image
+  calculateImage(an_image, imageVec, maxVal); // x kernel 
+  getFinalImage(an_image, imageVec, maxVal);  //final Image
 
   // saving the processed result to output file.
   if (!WriteImage(output_file, an_image)){
